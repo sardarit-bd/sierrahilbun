0aaf535eb2c2be8620e3380e47242f603f2d0fbc
shahadat <funkyd3v@gmail.com>
2026-02-15 17:51:20 +0600

    review added for admin

---
+++ app/Filament/Resources/ProductReviews/Pages/CreateProductReview.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Resources\ProductReviews\Pages;
+
+use App\Filament\Resources\ProductReviews\ProductReviewResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateProductReview extends CreateRecord
+{
+    protected static string $resource = ProductReviewResource::class;
+
+    
+}
