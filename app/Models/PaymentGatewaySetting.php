364368313fac04fcc4feeb89030e7c8dd2263da8
shahadat <funkyd3v@gmail.com>
2026-03-04 17:45:51 +0600

    pg asdfsdf

--- app/Models/PaymentGatewaySetting.php
+++ app/Models/PaymentGatewaySetting.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 
 class PaymentGatewaySetting extends Model
 {
@@ -22,4 +24,28 @@ class PaymentGatewaySetting extends Model
         'secret_key',
         'webhook_secret',
     ];
+
+    protected function secretKey(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value ? Crypt::decryptString($value) : null,
+            set: fn ($value) => $value ? Crypt::encryptString($value) : null,
+        );
+    }
+
+    protected function publicKey(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value ? Crypt::decryptString($value) : null,
+            set: fn ($value) => $value ? Crypt::encryptString($value) : null,
+        );
+    }
+
+    protected function webhookSecret(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value ? Crypt::decryptString($value) : null,
+            set: fn ($value) => $value ? Crypt::encryptString($value) : null,
+        );
+    }
 }
