238942c0f62c0c87f63a042c9bf5e569ccd3065c
shahadat <funkyd3v@gmail.com>
2026-03-06 12:10:39 +0600

    backend:admin panel UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,33 @@
+// import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+// import tailwindcss from '@tailwindcss/vite';
+// import react from '@vitejs/plugin-react';
+// import laravel from 'laravel-vite-plugin';
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: [
+//                 'resources/css/app.css', 
+//                 'resources/js/app.tsx', 
+//                 'resources/css/filament/admin/theme.css'],
+//             ssr: 'resources/js/ssr.tsx',
+//             refresh: true,
+//         }),
+//         react({
+//             babel: {
+//                 plugins: ['babel-plugin-react-compiler'],
+//             },
+//         }),
+//         tailwindcss(),
+//         wayfinder({
+//             formVariants: true,
+//         }),
+//     ],
+//     esbuild: {
+//         jsx: 'automatic',
+//     },
+// });
 import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
@@ -9,9 +39,10 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css', 
-                'resources/js/app.tsx', 
-                'resources/css/filament/admin/theme.css'],
-            ssr: 'resources/js/ssr.tsx',
+                'resources/css/filament/admin/theme.css',
+                'resources/js/app.tsx'      
+            ],
+            ssr: 'resources/js/ssr.tsx',     
             refresh: true,
         }),
         react({
